cf410826350de57cb341508a06b6f7b5dc9125e3
Pavel Kostitsyn <pavel.kostitsyn@nutnet.ru>
2024-08-29 17:59:17 +0400

    feat(DB): db config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=try-yii2',
+    'username' => 'try-yii2',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
